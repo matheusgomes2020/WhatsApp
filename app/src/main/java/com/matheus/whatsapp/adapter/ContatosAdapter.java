a82f0740cf3c4910086fea6a8a4ad95a48a2438e
matheusgomes2020 <matheusgomez2013@gmail.com>
2022-04-15 16:15:39 -0300

    aulas 232 e 233. Listando contatos.

---
+++ app/src/main/java/com/matheus/whatsapp/adapter/ContatosAdapter.java
@@ -0,0 +1,73 @@
+package com.matheus.whatsapp.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.matheus.whatsapp.R;
+import com.matheus.whatsapp.model.Usuario;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ContatosAdapter extends RecyclerView.Adapter<ContatosAdapter.MyViewHolder> {
+
+    private List<Usuario> contatos;
+    private Context context;
+
+    public ContatosAdapter(List<Usuario> listacontatos, Context c) {
+        this.contatos = listacontatos;
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemlista = LayoutInflater.from( parent.getContext() ).inflate(R.layout.adapter_contatos, parent, false );
+        return new MyViewHolder( itemlista );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Usuario usuario = contatos.get( position );
+
+        holder.nome.setText( usuario.getNome() );
+        holder.email.setText( usuario.getEmail() );
+
+        if ( usuario.getFoto() != null ){
+            Uri uri = Uri.parse( usuario.getFoto() );
+            Glide.with( context ).load( uri ).into( holder.foto );
+        }else {
+            holder.foto.setImageResource( R.drawable.padrao );
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contatos.size();
+    }
+
+    public class  MyViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView foto;
+        TextView nome, email;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            foto = itemView.findViewById(R.id.imageViewFotoConato);
+            nome = itemView.findViewById(R.id.textNomeContato);
+            email = itemView.findViewById(R.id.textEmailContato);
+        }
+    }
+}
--- app/src/main/java/com/matheus/whatsapp/fragment/ContatosFragment.java
+++ app/src/main/java/com/matheus/whatsapp/fragment/ContatosFragment.java
@@ -2,65 +2,98 @@ package com.matheus.whatsapp.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.matheus.whatsapp.R;
+import com.matheus.whatsapp.adapter.ContatosAdapter;
+import com.matheus.whatsapp.config.ConfiguracaoFirebase;
+import com.matheus.whatsapp.model.Usuario;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContatosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class ContatosFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView recyclerViewListaContatos;
+    private ContatosAdapter adapter;
+    private ArrayList<Usuario> listacontatos = new ArrayList<>();
+    private DatabaseReference usuarioRef;
+    private ValueEventListener valueEventListenerContatos;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public ContatosFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContatosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ContatosFragment newInstance(String param1, String param2) {
-        ContatosFragment fragment = new ContatosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+       View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+       //Configurações iniciais
+        recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
+        usuarioRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuarios");
+
+        //Configurar adapter
+        adapter = new ContatosAdapter( listacontatos, getActivity() );
+
+        //Configurar recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( getActivity() );
+        recyclerViewListaContatos.setLayoutManager( layoutManager );
+        recyclerViewListaContatos.setHasFixedSize( true );
+        recyclerViewListaContatos.setAdapter( adapter );
+
+
+       return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarContatos();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        usuarioRef.removeEventListener( valueEventListenerContatos );
+    }
+
+    public void  recuperarContatos(){
+
+        valueEventListenerContatos = usuarioRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for ( DataSnapshot dados : snapshot.getChildren() ){
+
+                    Usuario usuario = dados.getValue( Usuario.class );
+                    listacontatos.add( usuario );
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
 }
